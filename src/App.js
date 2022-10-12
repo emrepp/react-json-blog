f037b89a629fe8b235e48d5cd1ad0696194144dc
Emma Repp <emmarepp.draws@gmail.com>
2022-10-11 20:40:46 -1000

    routing

--- src/App.js
+++ src/App.js
@@ -1,11 +1,28 @@
 
 import './App.css';
+import {Routes, BrowserRouter, Route} from "react-router-dom";
+import Home from "./pages/Home";
+import AddEditBlog from ".pages/AddEditBlog";
+import Blog from ".pages/Blog";
+import About from ".pages/About";
+
+
 
 function App() {
   return (
-    <div className="App">
-      <h2>Blog</h2>
+    <BrowserRouter>
+     <div className="App">
+      <Routes>
+        <Route path="/" element = {<Home />} />
+        <Route path="/addBlog" element = {<AddEditBlog />} />
+        <Route path="/Blog/:id" element = {<Blog />} />
+        <Route path="/about" element = {<About />} />
+        <Route path="*" element={<NotFound/>}/>
+
+      </Routes>
     </div>
+    </BrowserRouter>
+   
   );
 }
 
